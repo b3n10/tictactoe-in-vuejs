885030466d11ea4675714fd337327b4e38e5fa6a
b3n10 <francisben.lleve@gmail.com>
2018-09-01 06:42:47 +0800

    Draw X using span

--- src/Box.vue
+++ src/Box.vue
@@ -3,9 +3,12 @@
 			:class="boxCls"
 			@click="$emit('userMove', boxKey)"
 			>
+
 			<p class="box__text"
 			:class="txtCls"
 			>
+			<span class="box__text-x1"></span>
+			<span class="box__text-x2"></span>
 			{{ text }}
 			</p>
 	</div>
@@ -19,12 +22,13 @@ export default {
 
 <style lang="scss">
 .box {
-	background-color: #c1c1c1;
-	width: 33.3%;
+	background-color: #f1f1f1;
+	width: 33%;
 	height: 33%;
 	display: inline-block;
 	border-right: 5px solid #000;
 	border-bottom: 5px solid #000;
+	border-radius: 5px;
 	box-sizing: border-box;
 	text-align: center;
 	overflow: hidden;
@@ -44,6 +48,7 @@ export default {
 	}
 
 	&__text {
+		position: relative;
 		width: 100%;
 		height: 100%;
 		margin: 0;
@@ -52,11 +57,29 @@ export default {
 		font: 110px/1.01 serif;
 		overflow: hidden;
 		cursor: pointer;
-		opacity: 0;
+		opacity: 1;
 		transition: opacity 1s;
 
 		&.marked {
 			opacity: 1;
+		}
+
+		&-x1, &-x2 {
+			position: absolute;
+			top: 12%;
+			left: 45%;
+			width: 10px;
+			height: 80%;
+			background-color: #565454;
+			border-radius: 5px;
+			transition: height 1s;
+		}
+
+		&-x1 {
+			transform: rotate(45deg);
+		}
+		&-x2 {
+			transform: rotate(135deg);
 		}
 	}
 }
